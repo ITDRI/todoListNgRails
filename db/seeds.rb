d6fcde232e7649e199ee4528539695582bf3f2e6
ITDRI <72489191+ITDRI@users.noreply.github.com>
2021-03-29 11:09:25 +0300

    test seed

--- db/seeds.rb
+++
@@ -1,6 +0,0 @@
-yml = File.join(Rails.root, 'db', 'seeds.yml')
-data = HashWithIndifferentAccess.new(YAML::load_file(yml))
-data[:projects].each do |project|
-  created_project = Project.create!(title: project[:title])
-  created_project.todos.create!(project[:todos])
-end
