6cf92de561fcd5fe56cfcd272ea984df52b07f53
ITDRI <72489191+ITDRI@users.noreply.github.com>
2021-03-29 12:09:12 +0300

    q

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   # root 'project#index'
   get 'todos' => 'todo#index'
   post 'todo' => 'todo#create'
-
-
   get 'projects' => 'project#index'
   put 'projects/:id/todo/:id' => 'project#update'
 end
