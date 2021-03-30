3874175b1ce99f467efb38ccb8e99f095502e1fe
ITDRI <72489191+ITDRI@users.noreply.github.com>
2021-03-30 16:01:44 +0300

    1'

--- ng-app/server.js
+++ ng-app/server.js
@@ -1,10 +1,10 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
+// function requireHTTPS(req, res, next) {
+//   // The 'x-forwarded-proto' check is for Heroku
+//   if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+//     return res.redirect('https://' + req.get('host') + req.url);
+//   }
+//   next();
+// }
 const express = require('express');
 const app = express();
 const port = 8080
@@ -18,4 +18,3 @@ app.get('/*', function(req, res) {
 });
 
 app.listen(port, () => console.log(`serv running http://localhost:${port}`));
-
