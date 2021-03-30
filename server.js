e5995d50033dc249d5a237e5b6598520b8ff0e1e
ITDRI <72489191+ITDRI@users.noreply.github.com>
2021-03-30 21:40:14 +0300

    w

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(requireHTTPS);
 
 app.use(express.static('./ng-app/dist/ng-app'));
 
-// app.get('/*', function(req, res) {
-//   res.sendFile('index.html', {root: 'ng-app/dist/ng-app/'}
-//   );
-// });
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'ng-app/dist/ng-app/'}
+  );
+});
 
 app.listen(process.env.PORT || 3000,
   () => console.log("Server is running..."));
