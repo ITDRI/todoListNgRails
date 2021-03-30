09651377ec28b6d48949643a5f45f2d0cd9298bc
ITDRI <72489191+ITDRI@users.noreply.github.com>
2021-03-30 19:52:27 +0300

    s

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 // }
 const express = require('ng-app/express');
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000
 // app.use(requireHTTPS);
 
 app.use(express.static('./ng-app/dist/ng-app'));
